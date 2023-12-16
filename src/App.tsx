c17ee423dd19bb6cd63424a291bdb36966278a84
Joyce Yu <joyceyu96@gmail.com>
2023-12-15 17:59:36 -0800

    Created thin button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+import ThinButton from './components/ThinButton'
+
 function App() {
 
   return (
     <>
-
+      <ThinButton text="Login" />
     </>
   )
 }
---
+++ src/components/ThinButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const ThinButton = (text:any) => {
+  return (
+      <Button variant="contained">{text}</Button>
+  )
+}
+
+export default ThinButton
