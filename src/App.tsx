d022ff08aaff60eb78329d53bb0bcd3e85f910c1
Yangyue Song <syangyue1990@gmail.com>
2023-12-27 12:52:07 -0800

    updated volunteertype form and collectionForm

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,30 @@ import './App.css';
 import Login from './components/Login.tsx';
 import Signup from './components/Signup.tsx';
 import ProfileInformationForm from './components/ProfileInformationForm.tsx';
+import VolunteerTypeForm from './components/VolunteerTypeForm.tsx';
+import CollectionInfoForm from './components/CollectionInfoForm.tsx';
+
 
 
 function App() {
 
   return (
 
-
     <div className='container'>
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="signup/">
-            <Route path="" element={<Signup />} />
-            <Route path="profile" element={<ProfileInformationForm />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="signup">
+          <Route path="" element={<Signup />} />
+          <Route path="profile">
+            <Route path="" element={<ProfileInformationForm />} />
+            <Route path="volunteertype" element={<VolunteerTypeForm />} />
+            <Route path="collectioninfoform" element={<CollectionInfoForm />} />
           </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
 
 
 
